2639e98270985ab716e8c59009fe350b94da19ce
Joakim <joakim.solvang.christiansen@gmail.com>
2023-11-10 10:40:27 +0100

    Fikset på mye utseende. La også til options knapper

--- ChatHandler.js
+++ ChatHandler.js
@@ -1,4 +1,33 @@
-setTimeout('Send_Message(false, "Hei! Hva kan jeg hjelpe deg med?");', 500);
+Send_Message(false, "Hei! Hva kan jeg hjelpe deg med?");
+
+Send_Options("HTML");
+Send_Options("CSS");
+Send_Options("Javascript");
+
+function Send_Options(name, human) {
+    if (name && !human) {
+        var chatBox = document.getElementById("chat-box");
+        var newOption = document.createElement("button");
+
+        newOption.textContent = name;
+        newOption.classList.add("option-button");
+
+        newOption.style.padding = "0.5em 1em";
+        newOption.style.fontSize = "14px";
+        newOption.style.width = "auto";
+        newOption.style.minWidth = "10%";
+        newOption.style.overflow = "hidden";
+        newOption.style.textOverflow = "ellipsis";
+        newOption.style.whiteSpace = "nowrap";
+        newOption.style.margin = "0 0 0 10px";
+
+        newOption.addEventListener("click", function () {
+            alert("Du klikket på " + name);
+        });
+
+        chatBox.appendChild(newOption);
+    }
+}
 
 // Message sending
 
@@ -6,22 +35,36 @@ function Send_Message(human, message) {
     if(human && message == "") {
         var userInput = document.getElementById("user-input").value;
         if (userInput !== "") {
-            Upload_Message("Du: " + userInput);
+            Upload_Message(userInput, human);
             document.getElementById("user-input").value = "";
         }
 
     } else if(message != "") {
-        Upload_Message("Bot: " + message);
+        Upload_Message(message, human);
     }
 }
 
+// Enter keycheck
+
+document.getElementById("user-input").addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        Send_Message(true, "");
+    }
+});
+
+
+
 // Upload messages
   
-function Upload_Message(message) {
+function Upload_Message(message, human) {
     var chatBox = document.getElementById("chat-box");
     var newMessage = document.createElement("div");
 
-    newMessage.textContent = message;
+    newMessage.classList.add(human ? "user-message" : "bot-message");
+
+    newMessage.textContent = (human ? "" : "Scott: ") + message;
+
     chatBox.appendChild(newMessage);
 }
+
   
